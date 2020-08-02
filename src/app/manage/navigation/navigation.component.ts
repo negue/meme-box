19de2163ca724c4b1463bc83b8d555c07c3435cd
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-08-02 21:27:44 +0100

    Renamed .css files to .scss

--- src/app/manage/navigation/navigation.component.ts
+++ src/app/manage/navigation/navigation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
   links = [
