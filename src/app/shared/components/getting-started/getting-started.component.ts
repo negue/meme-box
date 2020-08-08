0b9f7aef7974e26dd9ab747772acc25fecde5d32
negue <eugen.bolz@gmail.com>
2020-08-08 16:43:13 +0200

    Offline Mode + Dummy Data

--- src/app/shared/components/getting-started/getting-started.component.ts
+++ src/app/shared/components/getting-started/getting-started.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs";
 import {Clip, Screen} from "@memebox/contracts";
 import {AppQueries} from "../../../state/app.queries";
 import {DialogService} from "../dialogs/dialog.service";
+import {AppService} from "../../../state/app.service";
 
 @Component({
   selector: 'app-getting-started',
@@ -13,9 +14,11 @@ export class GettingStartedComponent implements OnInit {
   public mediaList$: Observable<Clip[]> = this.query.clipList$;
 
   public screenList$: Observable<Screen[]> = this.query.screensList$
+  public inOfflineMode$: Observable<boolean> = this.query.inOfflineMode$;
 
 
   constructor(private query: AppQueries,
+              private appService: AppService,
               private dialog: DialogService) {
   }
 
@@ -28,5 +31,9 @@ export class GettingStartedComponent implements OnInit {
 
   newScreen() {
     this.dialog.showScreenEditDialog({});
+  }
+
+  fillData() {
+    this.appService.fillDummyData();
   }
 }
