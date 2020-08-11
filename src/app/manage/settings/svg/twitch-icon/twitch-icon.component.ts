b6c7c475b4f414497ff9ef44bf0a5f49b3438777
Salma Alam-Naylor <salmaalamnaylor@gmail.com>
2020-08-11 20:40:14 +0100

    Refactored a little bit of Sass

---
+++ src/app/manage/settings/svg/twitch-icon/twitch-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-twitch-icon',
+  templateUrl: './twitch-icon.component.html',
+  styleUrls: ['./twitch-icon.component.scss']
+})
+export class TwitchIconComponent {
+}
