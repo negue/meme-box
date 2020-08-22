6e27adc1c96e530adeb6aa09ab5b3b36fc7c26a9
negue <eugen.bolz@gmail.com>
2020-08-22 18:31:58 +0200

    parse CSS + apply parent class + apply styles

--- src/app/target-screen/target-screen.component.ts
+++ src/app/target-screen/target-screen.component.ts
@@ -1,6 +1,8 @@
+import * as css from 'css';
+import {Rule} from 'css';
 import {Component, ElementRef, OnDestroy, OnInit, TrackByFunction} from '@angular/core';
 import {BehaviorSubject, combineLatest, Observable, Subject} from "rxjs";
-import {Clip, Dictionary} from "@memebox/contracts";
+import {Clip, Dictionary, ScreenClip} from "@memebox/contracts";
 import {distinctUntilChanged, filter, map, take, takeUntil} from "rxjs/operators";
 import {AppQueries} from "../state/app.queries";
 import {AppService} from "../state/app.service";
@@ -58,6 +60,15 @@ export class TargetScreenComponent implements OnInit, OnDestroy {
   mediaClipToShow$ = new BehaviorSubject<string>(null);
   clipToControlMap = new WeakMap<Clip, HTMLVideoElement | HTMLAudioElement | HTMLImageElement>();
 
+  screen$ = combineLatest([
+    this.screenId$,
+    this.appQuery.screenMap$
+  ]).pipe(
+    map(([screenId, screenMap]) => {
+      return screenMap[screenId];
+    })
+  );
+
   trackByKeyValue: TrackByFunction<KeyValue<string, CombinedClip>> = (index, item) => {
     return item.key;
   }
@@ -112,6 +123,25 @@ export class TargetScreenComponent implements OnInit, OnDestroy {
     });
 
     this.screenId$.next(this.route.snapshot.params.guid);
+
+    this.mediaClipMap$.pipe(
+      takeUntil(this._destroy$)
+    ).subscribe(mediaClipMap => {
+      for(const screenClipSettings of Object.values(mediaClipMap)){
+        const customCss = screenClipSettings.clipSetting.customCss
+          ? this.toCssAgain(screenClipSettings.clipSetting)
+          : '';
+
+        this.addOrUpdateStyleTag(screenClipSettings.clipSetting.id, customCss);
+      }
+    });
+
+    this.screen$.pipe(
+      takeUntil(this._destroy$),
+      filter(screen => !!screen)
+    ).subscribe(screen => {
+      this.addOrUpdateStyleTag(screen.id, screen.customCss);
+    });
   }
 
   addLog(load: string, $event: Event) {
@@ -133,9 +163,70 @@ export class TargetScreenComponent implements OnInit, OnDestroy {
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',0.34)';
   }
 
+  parseCss(screenClip: ScreenClip) {
+
+    var obj = css.parse(screenClip.customCss, {
+      silent: true
+    });
+    // css.stringify(obj, options);
+
+    obj.stylesheet.rules.forEach(rule => {
+      if (rule.type === 'rule') {
+        const normalRule = rule as Rule;
+
+        normalRule.selectors = normalRule.selectors.map(sel => {
+          const withSpace = !sel.includes('.clip-holder');
+
+          return `.${screenClip.id}${withSpace ? ' ': ''}${sel}`;
+        });
+      }
+    })
+
+    return obj;
+  }
+
+  addOrUpdateStyleTag(styleId: string, customCss: string) {
+    const head = document.getElementsByTagName('head')[0];
+    const allStyles = head.getElementsByTagName('style');
+    let style: HTMLStyleElement = null;
+
+    for (let styleIndex = 0; styleIndex < allStyles.length; styleIndex++) {
+      const styleInHeader = allStyles.item(styleIndex);
+
+      if (styleInHeader.id === styleId) {
+        style = styleInHeader;
+        break;
+      }
+    }
+
+    if (style == null) {
+      style = document.createElement('style');
+      style.id = styleId;
+      style.type = 'text/css';
+      head.appendChild(style);
+    }
+
+    if (style.childNodes.length > 0) {
+      console.info('Rules in the Style', styleId);
+      console.info('Childnodes', style.childNodes);
+
+      style.childNodes.forEach(child => {
+        console.info('Removing', child);
+        style.removeChild(child);
+      })
+    }
+
+    style.appendChild(document.createTextNode(customCss));
+  }
 
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
+  }
+
+  toCssAgain(screenClip: ScreenClip) {
+    const obj = this.parseCss(screenClip);
+
+    return css.stringify(obj);
   }
 }
