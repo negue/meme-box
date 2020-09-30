f2befc3c306f3cff17f9c0ddbb1d475cfe72fa69
negue <eugen.bolz@gmail.com>
2020-09-30 19:07:27 +0200

    use placeholder in mapFileInformations  - fixes #95

--- server/file.utilts.ts
+++ server/file.utilts.ts
@@ -2,10 +2,11 @@ import {fileEndingToType} from "../projects/utils/src/lib/files";
 import {basename, extname, resolve, sep} from "path";
 import {FileInfo} from "../projects/contracts/src/lib/types";
 import fs from "fs";
+import {SERVER_URL} from "../projects/contracts/src/lib/placeholders";
 
 const { readdir } = fs.promises;
 
-export async function getFiles(dir): Promise<string[]> {
+export async function getFiles(dir: string): Promise<string[]> {
   const dirents = await readdir(dir, { withFileTypes: true });
   const files = await Promise.all(dirents.map(async (dirent) => {
     const res = resolve(dir, dirent.name);
@@ -18,6 +19,12 @@ export function mapFileInformations (
   mediaFolder: string,
   currentAppPort: string,
   files: string[]): FileInfo[] {
+
+  // remove path separator at the end
+  if (mediaFolder.endsWith(sep)) {
+    mediaFolder = mediaFolder.substring(0, mediaFolder.length - sep.length);
+  }
+
   // files with information
   return files.map((fullPath: string) => {
     const ext = extname(fullPath);
@@ -25,12 +32,14 @@ export function mapFileInformations (
 
     const fileType = fileEndingToType(ext);
 
-    // TODO replace PORT with the "--port"
-    const apiUrl = fullPath
-      .replace(mediaFolder,
-        `http://localhost:${currentAppPort}/file`
-      ).split(sep).join('/');
+    const convertedPath = fullPath
+      .replace(mediaFolder, `${SERVER_URL}/file`);
 
+    // path separator, not URL
+    const splittedPath = convertedPath.split(sep);
+
+    const apiUrl = splittedPath.join('/');
+    
     return {
       fullPath,
       fileName,
