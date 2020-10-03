b9d976bce737ac0da17c0c3505b58637a9a25968
negue <eugen.bolz@gmail.com>
2020-10-03 20:03:07 +0200

    clean up test

--- server/validations.spec.ts
+++ server/validations.spec.ts
@@ -1,32 +1,18 @@
-import {objIsClip, objIsClipWithoutId} from "./validations";
-import {expect} from 'chai';
 import {describe} from 'mocha';
-import {Clip} from "../projects/contracts/src/lib/types";
-import {MediaType} from "../projects/contracts/src/lib/media.types";
 
+// todo add tests with the  express validator
 describe('type validations', () => {
 
   it('should be a clip - without id' , () => {
-    expect(objIsClipWithoutId({
-      name: 'New Clip',
-      path: 'someUrl',
-      type: MediaType.Video,
-      playLength: 1337
-    } as Clip)).to.equal(true);
+
   });
 
   it('should be a clip' , () => {
-    expect(objIsClip({
-      name: 'New Clip',
-      path: 'someUrl',
-      type: MediaType.Video,
-      playLength: 1337,
-      id: 'some-guid'
-    } as Clip)).to.equal(true);
+
   });
 
   it('should not be a clip' , () => {
-    expect(objIsClip({})).to.equal(false);
+
   });
 
 });
