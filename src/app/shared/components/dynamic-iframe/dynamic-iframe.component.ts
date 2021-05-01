d32b26281c78e7078e728db2b94ba08e8cd9e0cf
negue <negue@users.noreply.github.com>
2021-05-01 22:41:26 +0200

    Custom HTML Widgets v2 - Part 1 (#268)
    * Fullscreen Custom HTML Dialog
    
    * Toggle preview auto refresh
    
    * Save the current server port in sessionStorage
    
    * move the preview border to global styles and use it on media edit / preview
    
    * custom variables inside html widgets
    
    * subscribe to twitch events websocket and post it to the iframe window
    
    * import / export custom html widgets

--- src/app/shared/components/dynamic-iframe/dynamic-iframe.component.ts
+++ src/app/shared/components/dynamic-iframe/dynamic-iframe.component.ts
@@ -4,19 +4,26 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
   ViewChild
 } from '@angular/core';
-import {dynamicIframe, DynamicIframeContent} from "../../../../../projects/utils/src/lib/dynamicIframe";
+import {dynamicIframe, DynamicIframeContent} from "@memebox/utils";
+import {WebsocketHandler} from "../../../core/services/websocket.handler";
+import {AppConfig} from "@memebox/app/env";
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-dynamic-iframe',
   templateUrl: './dynamic-iframe.component.html',
   styleUrls: ['./dynamic-iframe.component.scss']
 })
-export class DynamicIframeComponent implements OnInit, OnChanges {
+export class DynamicIframeComponent implements OnInit, OnChanges, OnDestroy {
+  private wsHandler: WebsocketHandler;
+  private _destroy$ = new Subject();
 
   @ViewChild('targetIframe', {static: true})
   targetIframe: ElementRef<HTMLIFrameElement>;
@@ -27,21 +34,44 @@ export class DynamicIframeComponent implements OnInit, OnChanges {
   @Output()
   load = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+    this.wsHandler = new WebsocketHandler(AppConfig.wsBase+'/ws/twitch_events', 3000);
+  }
 
   ngOnInit(): void {
-    if (this.content) {
-      dynamicIframe(this.targetIframe.nativeElement, this.content);
-    }
+    this.handleContentUpdate();
+
+    this.wsHandler.onMessage$.pipe(
+      takeUntil(this._destroy$)
+    ).subscribe(twitchEvent => {
+      console.info({twitchEvent});
+      this.targetIframe.nativeElement.contentWindow.postMessage(twitchEvent, '*');
+    });
 
     this.load.emit();
+  }
+  ngOnDestroy() {
+    this._destroy$.next();
   }
 
   ngOnChanges({content}: SimpleChanges): void {
     if(content) {
       console.warn('UPDATING IFRAME');
-      dynamicIframe(this.targetIframe.nativeElement, content.currentValue);
+      this.handleContentUpdate();
     }
   }
 
+  private handleContentUpdate() {
+    if (this.content) {
+      dynamicIframe(this.targetIframe.nativeElement, this.content);
+
+      if (this.content.settings) {
+        if (this.content.settings.subscribeToTwitchEvent) {
+          this.wsHandler.connect();
+        } else {
+          this.wsHandler.stopReconnects();
+        }
+      }
+    }
+  }
 }
