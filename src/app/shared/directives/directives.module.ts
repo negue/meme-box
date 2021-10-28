d54c268da0a774c9e7f7e4125015ba7db928cb3f
negue <eugen.bolz@gmail.com>
2021-10-28 21:30:22 +0200

    ui: change shadow on hover

---
+++ src/app/shared/directives/directives.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {HoverClassDirective} from "./hover-class.directive";
+
+@NgModule({
+  exports: [
+    HoverClassDirective
+  ],
+  declarations: [
+    HoverClassDirective
+  ]
+})
+export class DirectivesModule {
+
+}
