34e6d07d4edca1dbd9a0feecf3d4912b91c2e4ee
negue <eugen.bolz@gmail.com>
2022-02-05 19:10:15 +0100

    highlight directive for json info

---
+++ src/app/pages/dashboard/highlight.directive.ts
@@ -0,0 +1,44 @@
+import {Directive, ElementRef, Input, OnDestroy} from '@angular/core';
+import {Subject} from "rxjs";
+import {HighlightService} from "@gewd/components/highlight-editor";
+import {distinctUntilChanged, takeUntil} from "rxjs/operators";
+
+@Directive({
+  selector: '[highlight]'
+})
+export class HighlightDirective implements OnDestroy {
+  private destroy$ = new Subject();
+  private textToHighlight$ = new Subject<string>()
+
+  @Input()
+  public set highlight(value: string) {
+    this.textToHighlight$.next(value);
+  }
+
+  @Input()
+  public lang: string;
+
+  constructor(
+    private element: ElementRef<HTMLElement>,
+    private highlightService: HighlightService
+  ) {
+    this.textToHighlight$.pipe(
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe(async currentTextToHighlight => {
+      console.info('RAW', currentTextToHighlight);
+
+      const highlightedHtml = await this.highlightService.highlightCode(
+        currentTextToHighlight, this.lang
+      );
+      console.info('HTML', highlightedHtml);
+
+      this.element.nativeElement.innerHTML = highlightedHtml;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
