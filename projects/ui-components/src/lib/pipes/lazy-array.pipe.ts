c174aa487f8467484561a30ba25df2f4ae024e44
negue <eugen.bolz@gmail.com>
2022-03-10 18:10:47 +0100

    fix missing elements in multiple views

--- projects/ui-components/src/lib/pipes/lazy-array.pipe.ts
+++ projects/ui-components/src/lib/pipes/lazy-array.pipe.ts
@@ -1,16 +1,34 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Observable, of} from "rxjs";
+import {ChangeDetectorRef, OnDestroy, Pipe, PipeTransform} from '@angular/core';
+import {Observable, of, Subject} from "rxjs";
 import {lazyArray} from "@gewd/utils/rxjs";
+import {auditTime, takeUntil, tap} from "rxjs/operators";
 
 @Pipe({
   name: 'lazyArray$'
 })
-export class LazyArrayPipe implements PipeTransform {
+export class LazyArrayPipe implements PipeTransform, OnDestroy {
+  private _triggerRefresh = new Subject<void>();
+  private _destroy$ = new Subject<void>();
+
+  constructor(private cd: ChangeDetectorRef) {
+    this._triggerRefresh.pipe(
+      takeUntil(this._destroy$),
+      auditTime(50)
+    ).subscribe(() => {
+      cd.detectChanges();
+    });
+  }
 
   transform<TItem>(values: TItem[], delayBetween: number, amountOfItems: number): Observable<TItem[]> {
     return of(values).pipe(
-      lazyArray(delayBetween, amountOfItems)
+      lazyArray(delayBetween, amountOfItems),
+      tap(() => this._triggerRefresh.next()),
     );
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 
 }
