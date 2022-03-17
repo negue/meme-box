e94a81cba702507606658ee9ab1a9ab7fd996ec0
negue <eugen.bolz@gmail.com>
2022-03-17 17:51:04 +0100

    cleanup

--- projects/logic-step-ui/src/lib/logic-call/logic-call.component.ts
+++ projects/logic-step-ui/src/lib/logic-call/logic-call.component.ts
@@ -1,18 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {LogicEditorComponent} from "../logic-editor/logic-editor.component";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'logic-call',
   templateUrl: './logic-call.component.html',
   styleUrls: ['./logic-call.component.css']
 })
-export class LogicCallComponent implements OnInit {
-
-  constructor(
-    private logicContext: LogicEditorComponent
-  ) { }
-
-  ngOnInit(): void {
-  }
+export class LogicCallComponent {
 
 }
