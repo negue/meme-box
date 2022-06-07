43ee7db5504938bdc5d864c2e843cdf6fac9662c
negue <eugen.bolz@gmail.com>
2022-06-07 19:24:49 +0200

    some type changes

--- server/providers/twitch/twitch-trigger-handler.service.ts
+++ server/providers/twitch/twitch-trigger-handler.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, ProviderScope, ProviderType } from "@tsed/di";
-import { TwitchConnector } from "./twitch.connector";
-import { Inject } from "@tsed/common";
-import { PERSISTENCE_DI } from "../contracts";
-import { Persistence } from "../../persistence";
-import { Action, AllTwitchEvents, Dictionary, TriggerActionOrigin, TwitchTriggerCommand } from "@memebox/contracts";
-import { TwitchLogger } from "./twitch.logger";
-import { isAllowedToTrigger } from "./twitch.utils";
-import { getCommandsOfTwitchEvent, getLevelOfTags } from "./twitch.functions";
-import { ExampleTwitchCommandsSubject } from "../../shared";
-import { ActionQueueEventBus } from "../actions/action-queue-event.bus";
-import { uuid } from "@gewd/utils";
-import { TwitchQueueEventBus } from "./twitch-queue-event.bus";
+import {Injectable, ProviderScope, ProviderType} from "@tsed/di";
+import {TwitchConnector} from "./twitch.connector";
+import {Inject} from "@tsed/common";
+import {PERSISTENCE_DI} from "../contracts";
+import {Persistence} from "../../persistence";
+import {Action, Dictionary, TriggerActionOrigin, TwitchTriggerCommand} from "@memebox/contracts";
+import {TwitchLogger} from "./twitch.logger";
+import {isAllowedToTrigger} from "./twitch.utils";
+import {getCommandsOfTwitchEvent, getLevelOfTags} from "./twitch.functions";
+import {ExampleTwitchCommandsSubject} from "../../shared";
+import {ActionQueueEventBus} from "../actions/action-queue-event.bus";
+import {uuid} from "@gewd/utils";
+import {TwitchQueueEventBus} from "./twitch-queue-event.bus";
 
 // skipcq: JS-0579
 @Injectable({
@@ -41,7 +41,7 @@ export class TwitchTriggerHandler {
       .subscribe(twitchEvent => {
         const foundCommandsIterator = getCommandsOfTwitchEvent(
           this._twitchConnector.twitchSettings,
-          twitchEvent as AllTwitchEvents
+          twitchEvent
         );
 
         for (const command of foundCommandsIterator) {
