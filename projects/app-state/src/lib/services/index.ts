b52bf55c06e60f00ba61c66b8beb14d2bab7448c
negue <eugen.bolz@gmail.com>
2022-06-07 18:40:44 +0200

    dashboard always show screen reload

--- projects/app-state/src/lib/services/index.ts
+++ projects/app-state/src/lib/services/index.ts
@@ -3,3 +3,4 @@ export * from './snackbar.service';
 export * from './network-interfaces.service';
 export * from './memebox-websocket.service';
 export * from './settings.service';
+export * from './obs.service';
